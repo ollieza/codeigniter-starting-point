8cfdaf56f440cbac6f0d917f1c9069dbe06c99c6
Ollie Rattue <orattue@toomanytabs.com>
2011-02-14 16:11:13 +0100

    Added useful countries db schema and data

---
+++ CodeIgniter_1.7.3/schema/countries-schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `countries`;
+CREATE TABLE IF NOT EXISTS `countries` (
+  `country_iso` char(2) NOT NULL,
+  `country_name` varchar(80) NOT NULL,
+  `country_printable_name` varchar(80) NOT NULL,
+  `country_iso3` char(3) default NULL,
+  `country_numcode` smallint(6) default NULL,
+  PRIMARY KEY  (`iso`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
