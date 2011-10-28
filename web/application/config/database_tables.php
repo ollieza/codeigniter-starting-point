8ee1346efa74127c387083c597eb79ef95b5ae8f
Ollie Rattue <orattue@toomanytabs.com>
2011-10-28 18:18:01 +0100

    Added database tables config file and links library to make these settings accessible to models

---
+++ web/application/config/database_tables.php
@@ -0,0 +1,29 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| Database table settings
+|--------------------------------------------------------------------------
+|
+| Table names
+|
+*/
+
+// Autoresponders
+
+$config['autoresponders'] 				= 'autoresponders';
+$config['autoresponder_log']			= 'autoresponder_log';
+
+// Standard data
+
+$config['ci_sessions'] 					= 'ci_sessions';
+$config['countries'] 					= 'countries';
+
+// SimpleLoginSecure
+
+$config['users'] 						= 'users';
+$config['users_password_reset'] 		= 'users_password_reset';
+
+// App specific
+
+/* End of file database_tables.php */
+/* Location: ./application/config/database_tables.php */
