581282460734bc02a07c46edfac99112d639284f
Ollie Rattue <orattue@toomanytabs.com>
2011-10-29 00:32:16 +0100

    Autoloading libraries, config, and helpers

--- web/application/config/autoload.php
+++ web/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'form_validation', 'session', 'links_library', 'SimpleLoginSecure', 'autoresponder');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'html', 'form', 'debug_helper', 'page_helper', 'tax_helper', 'time_helper', 'template_helper');
 
 
 /*
@@ -80,7 +80,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('environment_settings');
 
 
 /*
