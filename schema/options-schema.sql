833818c4e6effbfa1a103a187fc5d0ff2e41e28f
Ollie Rattue <orattue@toomanytabs.com>
2011-10-28 23:30:47 +0100

    Removed options schema and crud model entry in readme documentation

--- schema/options-schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `options`;
-CREATE TABLE IF NOT EXISTS `options` (
-  `option_id` int(40) NOT NULL auto_increment,
-  `option_name` varchar(1000) NOT NULL,
-  `option_value` varchar(1000) NOT NULL,
-  PRIMARY KEY  (`option_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
