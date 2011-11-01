d3849a762c06d40213f22e1799e1ceb6947d0bd5
Kos Korolev <kos.korolev@gmail.com>
2011-11-02 00:28:20 +0600

    updated base.php and head.php according latest h5bp.com changes

--- web/application/views/templates/default/base.php
+++ web/application/views/templates/default/base.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <?php /* paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ */ ?>
-<!--[if lt IE 7 ]> <html class="no-js ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]--> 
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<?php /* Consider adding a manifest.appcache: h5bp.com/d/Offline */ ?>
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <?php
 $this->load->view('templates/default/head', $this->data);
 echo generate_body_tag($body_id, $body_class)."\n\t";
--- web/application/views/templates/default/head.php
+++ web/application/views/templates/default/head.php
@@ -5,13 +5,15 @@
 	<meta name="keywords" content="<?php echo META_KEYWORDS; ?>" />
 
 	<?php /* Mobile viewport optimized: j.mp/bplateviewport */ ?>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<?php /* CSS: implied media="all" */ ?>
 	<link rel="stylesheet" href="/_/css/style.css?<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/_/css/style.css'); ?>">
-
+	<?php /* More ideas for your <head> here: h5bp.com/d/head-Tips */ ?>
 	<?php if( $body_id != 'Login' ): ?>
-	<?php /* All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects */ ?>
-	<script src="/_/js/libs/modernizr-1.7.min.js"></script>
+	<?php /* All JavaScript at the bottom, except this Modernizr build incl. Respond.js
+			 Respond is a polyfill for min/max-width media queries. Modernizr enables HTML5 elements & feature detects;
+			 for optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ */ ?>
+		<script src="/_/js/libs/modernizr-2.0.6.min.js"></script>
 	<?php endif; ?>
 </head>
