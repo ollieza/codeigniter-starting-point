5ed492025f7b593b2b9c397c3d43b4be6ddf2246
Ollie Rattue <orattue@toomanytabs.com>
2011-12-05 14:11:40 +0000

    Removed template data parameter as we are using load->vars() function in template library

--- web/application/views/templates/default/base.php
+++ web/application/views/templates/default/base.php
@@ -6,10 +6,10 @@
 <?php /* Consider adding a manifest.appcache: h5bp.com/d/Offline */ ?>
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <?php
-$this->load->view('templates/default/head', $this->data);
+$this->load->view('templates/default/head');
 echo generate_body_tag($body_id, $body_class)."\n\t";
 echo '<div class="container clearfix">' . "\n\t";
-$this->load->view('templates/default/header', $this->data);
+$this->load->view('templates/default/header');
 echo '<div id="main" role="main" class="clearfix">' . "\n\t";
 
 if (is_array($path))
@@ -25,9 +25,9 @@ else
 }
 	
 echo '</div>' . "\n";
-$this->load->view('templates/default/footer', $this->data);
+$this->load->view('templates/default/footer');
 echo '</div>' . "\n";
-$this->load->view('templates/default/footer_js', $this->data);
+$this->load->view('templates/default/footer_js');
 echo '</body>' . "\n";
 ?>
 </html>
